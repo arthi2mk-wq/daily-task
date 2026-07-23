7f4ab9c3480c7168a97745d936d0e9217328e97d
Arthi <arthi2mk@gmail.com>
2026-07-23 17:30:04 +0530

    Add files via upload

---
+++ day7(ES6,storage)/exp.js
@@ -0,0 +1,13 @@
+export const add=(a,b)=>{
+    let sum=a+b
+    return "Addition of a &b:  "+ sum
+}
+export const sub=(a,b)=>{
+    return "subtraction of a&b :  "+ a-b
+}
+export const mul=(a,b)=>{
+    return "Multiply of a&b:  "+ a*b
+}
+export const div=(a,b)=>{
+    return "Division of a&b:  "+a/b
+}
